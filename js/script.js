28a2b4f6013c62c7be7a3e9f17fb7be487258817
Lauren Judge <l.m.judge@hotmail.com>
2019-04-06 17:21:22 +1300

    scroll effect added

--- js/script.js
+++ js/script.js
@@ -11,4 +11,20 @@ $(document).ready(function() {
         offset: '60px;'
     });
 
+
+    /* Navigation scroll */
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html,body').animate({
+                scrollTop: target.offset().top
+              }, 1000);
+              return false;
+            }
+          }
+        });
+      });
 });
